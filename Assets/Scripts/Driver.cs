b52863161a3143b3ccf893a6a9c460b50d0d56d0
nikolay5r <nradilov30@gmail.com>
2024-10-08 12:33:38 +0300

    Added driving logic

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float steerSpeed = 10.0f;
+    [SerializeField] private float moveSpeed = 10.0f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Translate(0, moveAmount, 0);
     }
 }
